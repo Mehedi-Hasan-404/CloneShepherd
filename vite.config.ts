a6d12cd9186dbc0c9c089c3876e80351bdccf4e2
Mehedi-Hasan-404 <880188mm@gmail.com>
2025-09-30 18:40:50 +0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,11 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
+    allowedHosts: ["8080-i52tjkx2xbqbfmh06vovo-6532622b.e2b.dev"], // 👈 add this
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
