36cc316d08b03276ef4696130c374921295dedd8
Mehedi-Hasan-404 <880188mm@gmail.com>
2025-10-03 07:52:18 +0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,17 @@
-// vite.config.ts
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
+  server: {
+    host: "0.0.0.0",
+    port: 5000,
+    allowedHosts: ["*"],
+  },
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
-  },
-  // Only used in `vite dev`, ignored in `vite build`
-  server: {
-    port: 5000,
-    host: true, // only binds to 0.0.0.0 if CLI uses --host
-    // allowedHosts defaults to "auto" (safe)
   },
 });
