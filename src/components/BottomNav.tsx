0468161226671d7b2a96e35efc1f6013c607cfae
Mehedi-Hasan-404 <880188mm@gmail.com>
2025-10-03 08:08:10 +0600

    Update BottomNav.tsx

--- src/components/BottomNav.tsx
+++ src/components/BottomNav.tsx
@@ -1,5 +1,5 @@
 import { Home, Star } from 'lucide-react';
-import { Link, useLocation } from 'react-router-dom';
+import { Link, useLocation } from 'wouter';
 
 const BottomNav = () => {
   const location = useLocation();
