593352d0136a1e537c81620f2c8177f257721dcc
Mehedi-Hasan-404 <880188mm@gmail.com>
2025-10-09 07:38:59 +0600

    Update use-mobile.tsx

--- src/hooks/use-mobile.tsx
+++ src/hooks/use-mobile.tsx
@@ -6,17 +6,17 @@ export function useIsMobile() {
   const [isMobile, setIsMobile] = React.useState<boolean | undefined>(undefined);
 
   React.useEffect(() => {
-    const compute = () => {
-      setIsMobile(window.innerWidth < MOBILE_BREAKPOINT);
-    };
-
-    compute();
-    window.addEventListener("resize", compute);
-    window.addEventListener("orientationchange", compute);
-    return () => {
-      window.removeEventListener("resize", compute);
-      window.removeEventListener("orientationchange", compute);
-    };
+    const isTouchDevice = 'ontouchstart' in window || navigator.maxTouchPoints > 0;
+    const userAgent = navigator.userAgent.toLowerCase();
+    const isMobileUA = /android|webos|iphone|ipad|ipod|blackberry|iemobile|opera mini/i.test(userAgent);
+    
+    const initialWidth = window.innerWidth;
+    const initialHeight = window.innerHeight;
+    const smallestDimension = Math.min(initialWidth, initialHeight);
+    
+    const isMobileDevice = isMobileUA || (isTouchDevice && smallestDimension < MOBILE_BREAKPOINT);
+    
+    setIsMobile(isMobileDevice);
   }, []);
 
   return !!isMobile;
